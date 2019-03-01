3752e74e4c7de187d71bdc4189c3f815139993fb
Michael Bonner <michaelb@thinkful.com>
2019-03-01 17:13:06 -0500

    migrations & seeds

---
+++ noteful_server/seeds/seed.noteful_notes.sql
@@ -0,0 +1,14 @@
+INSERT INTO noteful_notes (id, note_name, folder_id, content)
+VALUES
+  (1, 'Dogs', '1', 'lorem ipsum'),
+  (2, 'Cats', '2', 'lorem ipsum'),
+  (3, 'Pigs', '2', 'lorem ipsum'),
+  (4, 'Birds', '1', 'lorem ipsum'),
+  (5, 'Bears', '1', 'lorem ipsum'),
+  (6, 'Horses', '2','lorem ipsum'),
+  (7, 'Tigers', '3', 'lorem ipsum'),
+  (8, 'Wolves', '3', 'lorem ipsum'),
+  (9, 'Elephants', '2', 'lorem ipsum'),
+  (10, 'Lions', '1','lorem ipsum');
+
+ --   ALTER SEQUENCE noteful_notes RESTART WITH 11;
